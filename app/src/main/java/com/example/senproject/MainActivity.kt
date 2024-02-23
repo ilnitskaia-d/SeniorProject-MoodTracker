dbc5a741edb0bbde896d2ee2095c22656ff11b7b
ilnitskaia-d <ilnitskaia_d@auca.kg>
2024-02-23 19:25:36 +0600

    Add bottom menu (closes #11)

---
+++ app/src/main/java/com/example/senproject/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.senproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.senproject.databinding.ActivityMainBinding
+import com.example.senproject.databinding.EntryListBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.container)
+        setupWithNavController(binding.botNav, navController)
+    }
+}
--- app/src/main/java/com/example/senproject/ui/EntryList.kt
+++ app/src/main/java/com/example/senproject/ui/EntryList.kt
@@ -5,26 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.senproject.R
 import com.example.senproject.database.MoodEntry
 import com.example.senproject.database.MoodState
 import com.example.senproject.databinding.EntryListBinding
 import com.example.senproject.ui.adapter.EntryListAdapter
 
 class EntryList : Fragment() {
-    val list = listOf<MoodEntry>(
+    private val list = listOf<MoodEntry>(
         MoodEntry(moodState = MoodState.GOOD, time = "1:00"),
         MoodEntry(moodState = MoodState.GOOD, time = "2:00"),
         MoodEntry(moodState = MoodState.OK, time = "3:00"),
         MoodEntry(moodState = MoodState.BAD, time = "4:00")
     )
 
-    var binding: EntryListBinding? = null
+    private var binding: EntryListBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = EntryListBinding.inflate(layoutInflater)
         return binding!!.root
     }
